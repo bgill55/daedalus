ff128e87b1a3d59b5c256b6c8105c367baf03535
bgill55_dev <bricam55@gmail.com>
2026-09-01 14:55:01 -0600

    fix: disable native file watchers on Windows to prevent fs-event assertion errors

--- vitest.setup.ts
+++ vitest.setup.ts
@@ -1,3 +1,10 @@
+// Set Windows file watcher to polling mode to avoid fs-event assertion errors
+if (process.platform === 'win32') {
+  process.env.CHOKIDAR_USEPOLLING = 'true';
+  process.env.CHOKIDAR_POLL_INTERVAL = '100';
+  process.env.WATCHMAN_DISABLED = 'true';
+}
+
 process.on('uncaughtException', (err) => {
   process.stderr.write(`[vitest-worker PID=${process.pid}] UNCAUGHT EXCEPTION: ${err?.stack ?? err}\n`);
 });
